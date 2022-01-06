813690e6dee07b2b0ddd3d64c91a69f92dd611d0
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-01-06 22:00:26 +0100

    Preparing release 1.0.1
    [ci skip]

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/erral/doradetector",
-    version="1.0.1.dev0",
+    version="1.0.1",
     zip_safe=False,
 )
